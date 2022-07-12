de702d8a43e24f0d2d083146982542f4a8ec030f
darondel-yoobic <darondel@yoobic.com>
2022-07-12 22:06:18 +0100

    feat(storybook): allow using JSX stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
 module.exports = {
   addons: ['@storybook/addon-essentials'],
+  babel: (options) => ({
+    ...options,
+    plugins: [...options.plugins, [require.resolve('@babel/plugin-transform-react-jsx'), { pragma: 'h' }, 'preset']],
+    presets: [['@babel/typescript', { jsxPragma: 'h' }]]
+  }),
   framework: '@storybook/html',
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+import { getHostRef, registerHost, renderVdom } from '@stencil/core/internal/client';
+
 import { defineCustomElements } from '../loader';
 
 defineCustomElements();
+
+export const decorators = [
+  (Story) => {
+    const host = document.createElement('div');
+
+    registerHost(host, { $flags$: 0, $tagName$: host.tagName.toLowerCase() });
+    renderVdom(getHostRef(host), Story());
+
+    return host.children[0];
+  }
+];
