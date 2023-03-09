a3ec11ed6c52a4c6f211016e538f85afcc025600
ageffroy-yoobic <ageffroy@yoobic.com>
2023-03-09 10:51:08 +0000

    feat(ytb): improve button atom and diversify classes

---
+++ src/youtube/1. Atoms/button/button.tsx
@@ -0,0 +1,19 @@
+import { Component, h,Prop } from '@stencil/core';
+
+@Component({
+  tag: 'button-ytb',
+  styleUrl: 'button.scss',
+  shadow: true
+})
+
+export class ButtonYTB {
+@Prop() class:string;
+
+  render() {
+    return (
+      <button class={this.class}>
+        <slot />
+      </button>
+    );
+  }
+}
