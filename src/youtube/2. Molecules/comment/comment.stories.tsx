9087a4a3af6233130eb26f9295f8a8d94c0335d7
ageffroy-yoobic <ageffroy@yoobic.com>
2023-03-14 17:51:17 +0000

    feat(ytb) implement one  comment section

---
+++ src/youtube/2. Molecules/comment/comment.stories.tsx
@@ -0,0 +1,11 @@
+import { h } from '@stencil/core';
+
+export default {
+  title: 'Molecules/Comment',
+  component: 'navbar',
+  parameters: {
+    viewmode: 'story'
+  }
+};
+
+export const Comment = () => <com-container></com-container>
